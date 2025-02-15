ae85dd444dd1d5c3c0d96b94e61f0d5194a7e688
JuanjDes <juanjguapo@gmail.com>
2025-02-15 20:04:46 +0100

    Clase MongoDB. Corregido errores

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,23 @@ const User = require('../models/Users.js');
 
 router.post('/create', async (req, res) => {
     try {
-
+        const user = await User.create(req.body);
+        res.status(200).send(user);
     } catch(error) {
         console.error(error);
         res.status(500).send({message: 'There was an error creating the user'});
+    }
+});
+
+router.get('/get-all', async (req, res) => {
+    try {
+        const users = await User.find({});
+        // mostramos todos los usuarios
+        res.status(200).send(users);
+        
+    } catch(error) {
+        console.error(error);
+        res.status(500).send({message: 'There was an error retrieving the users'});
     }
 });
 
